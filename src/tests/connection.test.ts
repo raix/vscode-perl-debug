61e2096866745338f73934633cb3be6ceaa3a229
Morten Henriksen <mh@gi2.dk>
2016-12-03 22:14:24 +0100

    add debug info for variable inspection in tests

--- src/tests/connection.test.ts
+++ src/tests/connection.test.ts
@@ -214,6 +214,8 @@ suite('Perl debugger connection', () => {
 
 		suite('getVariableList', () => {
 			test('Should get more scope variables types', async function() {
+				conn.debug = true;
+				conn.streamCatcher.debug = true;
 				await conn.launchRequest(FILE_TEST_PL, DATA_ROOT, []);
 				await conn.setBreakPoint(23, FILE_MODULE);
 
